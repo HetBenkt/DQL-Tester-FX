10c8b3acbfbdd4e70fd8704d4261d283927658f4
Antal Bos <HetBenkt@gmail.com>
2018-09-07 00:05:49 +0200

    Do a clean fireEvent action Next is adding data to update stuff on the other side

--- src/main/java/nl/bos/controllers/InputPane.java
+++ src/main/java/nl/bos/controllers/InputPane.java
@@ -2,6 +2,7 @@ package nl.bos.controllers;
 
 import com.documentum.fc.common.DfException;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,6 +10,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import lombok.Getter;
 import lombok.extern.java.Log;
 import nl.bos.Repository;
@@ -16,7 +18,7 @@ import nl.bos.Repository;
 import java.io.IOException;
 
 @Log
-public class InputPane {
+public class InputPane implements EventHandler<WindowEvent> {
     @Getter
     private static Stage loginStage;
     @FXML
@@ -41,6 +43,7 @@ public class InputPane {
         loginStage.setTitle("Documentum Login");
         VBox loginPane = FXMLLoader.load(getClass().getResource("/nl/bos/views/LoginPane.fxml"));
         loginStage.setScene(new Scene(loginPane));
+        loginStage.setOnCloseRequest(this);
         loginStage.showAndWait();
     }
 
@@ -49,5 +52,9 @@ public class InputPane {
         log.info(String.valueOf(actionEvent.getSource()));
         Repository.disconnect();
         System.exit(0);
+    }
+
+    public void handle(WindowEvent event) {
+        log.info(String.valueOf(event.getSource()));
     }
 }
