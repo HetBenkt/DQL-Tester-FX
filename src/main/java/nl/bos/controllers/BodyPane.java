d1309798e0c20db687038a9018edbc5e7f9fdbba
Antal Bos <HetBenkt@gmail.com>
2018-09-11 17:14:53 +0200

    Save history queries in a properties file for next time

--- src/main/java/nl/bos/controllers/BodyPane.java
+++ src/main/java/nl/bos/controllers/BodyPane.java
@@ -2,6 +2,8 @@ package nl.bos.controllers;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -13,9 +15,12 @@ import javafx.scene.layout.VBox;
 import lombok.Getter;
 import lombok.extern.java.Log;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
 
 @Log
 public class BodyPane implements Initializable, ChangeListener {
@@ -29,16 +34,50 @@ public class BodyPane implements Initializable, ChangeListener {
     private ChoiceBox cmbHistory;
     @FXML
     private TableView tbResult;
+    @Getter
+    private Properties history = new Properties();
 
     public void initialize(URL location, ResourceBundle resources) {
         try {
             BorderPane inputPane = FXMLLoader.load(getClass().getResource("/nl/bos/views/InputPane.fxml"));
             vboxBody.getChildren().add(inputPane);
-
-            cmbHistory.getSelectionModel().selectedIndexProperty().addListener(this);
         } catch (IOException e) {
             log.info(e.getMessage());
         }
+        cmbHistory.getSelectionModel().selectedIndexProperty().addListener(this);
+        loadHistory();
+    }
+
+    private void loadHistory() {
+        try {
+            InputStream in = new FileInputStream("history.properties");
+            if (in != null) {
+                history.load(in);
+                List<String> statements = getPropertyList(history, "q");
+                ObservableList value = FXCollections.observableList(statements);
+                cmbHistory.setItems(value);
+            }
+        } catch (IOException e) {
+            log.info(e.getMessage());
+            File file = new File("history.properties");
+            try {
+                if (file.createNewFile())
+                    log.info("History file created.");
+            } catch (IOException e1) {
+                log.info(e1.getMessage());
+            }
+
+        }
+    }
+
+    private List<String> getPropertyList(Properties properties, String name) {
+        List<String> result = new ArrayList<String>();
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            if (((String) entry.getKey()).startsWith(name)) {
+                result.add((String) entry.getValue());
+            }
+        }
+        return result;
     }
 
     public void changed(ObservableValue observable, Object oldValue, Object newValue) {
