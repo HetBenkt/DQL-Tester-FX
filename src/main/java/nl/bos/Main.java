286b075f83b1981cd74e683f3e8b9e2e493f24f0
Antal Bos <antal.bos@amplexor.com>
2018-09-11 13:55:23 +0200

    First query can be executed; - save it in the history - create a disconnect button

--- src/main/java/nl/bos/Main.java
+++ src/main/java/nl/bos/Main.java
@@ -6,10 +6,13 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import lombok.Getter;
 import lombok.extern.java.Log;
 
 @Log
 public class Main extends Application {
+    @Getter
+    private static FXMLLoader bodyPaneLoader;
 
     public static void main(String[] args) {
         launch(args);
@@ -18,7 +21,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         BorderPane rootPane = FXMLLoader.load(getClass().getResource("/nl/bos/views/RootPane.fxml"));
-        VBox bodyLayout = FXMLLoader.load(getClass().getResource("/nl/bos/views/BodyPane.fxml"));
+        bodyPaneLoader = new FXMLLoader(getClass().getResource("/nl/bos/views/BodyPane.fxml"));
+        VBox bodyLayout = bodyPaneLoader.load();
 
         rootPane.setCenter(bodyLayout);
 
--- src/main/java/nl/bos/controllers/BodyPane.java
+++ src/main/java/nl/bos/controllers/BodyPane.java
@@ -3,8 +3,12 @@ package nl.bos.controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import lombok.Getter;
 import lombok.extern.java.Log;
 
 import java.io.IOException;
@@ -15,6 +19,13 @@ import java.util.ResourceBundle;
 public class BodyPane implements Initializable {
     @FXML
     private VBox vboxBody;
+    @FXML
+    @Getter
+    private TextArea taStatement;
+    @FXML
+    private ChoiceBox cmbHistory;
+    @FXML
+    private TableView tbResult;
 
     public void initialize(URL location, ResourceBundle resources) {
         try {
