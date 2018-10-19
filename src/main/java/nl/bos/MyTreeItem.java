441c4cb703f877457170c3d979072fef19c7512a
Antal Bos <HetBenkt@gmail.com>
2018-10-19 16:17:59 +0200

    Connected the first repository objects in the repository TreeView. Now finetuning with icons, naming and correct queries

--- src/main/java/nl/bos/MyTreeItem.java
+++ src/main/java/nl/bos/MyTreeItem.java
@@ -1,25 +1,64 @@
 package nl.bos;
 
+import com.documentum.fc.client.IDfCollection;
+import com.documentum.fc.common.DfException;
 import javafx.scene.control.TreeItem;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import lombok.Data;
+import lombok.extern.java.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
+@Log
 public class MyTreeItem extends TreeItem<String> {
 
     private String type;
+    private String path;
+    private Repository repositoryCon = Repository.getInstance();
 
-    public MyTreeItem(String name, String type, ImageView imageView) {
+    public MyTreeItem(String name, String type, ImageView imageView, String path) {
         super(name, imageView);
         this.type = type;
+        this.path = path;
     }
 
     public boolean isDirectory() {
         return type.equals("repository") || type.equals("cabinet") || type.equals("folder");
     }
 
-    public String[] getFolders() {
-        String[] result = {"test", "test2"};
-        return result;
+    public List<MyTreeItem> listObjects(MyTreeItem parent) {
+        List<MyTreeItem> children = new ArrayList();
+
+        try {
+            if (parent.getType().equals("repository")) {
+                IDfCollection cabinets = repositoryCon.query("select r_object_id, object_name, is_private from dm_cabinet order by object_name");
+                while (cabinets.next()) {
+                    MyTreeItem child = new MyTreeItem(cabinets.getString("object_name"), "cabinet", new ImageView(
+                            new Image(getClass().getClassLoader().getResourceAsStream("cabinet_16.png"))), String.format("%s/%s", parent.getPath(), cabinets.getString("object_name")));
+                    children.add(child);
+                }
+            } else if (parent.getType().equals("cabinet")) {
+                IDfCollection folders = repositoryCon.query("select r_object_id, object_name from dm_folder order by object_name");
+                while (folders.next()) {
+                    MyTreeItem child = new MyTreeItem(folders.getString("object_name"), "folder", new ImageView(
+                            new Image(getClass().getClassLoader().getResourceAsStream("folder_16.png"))), String.format("%s/%s", parent.getPath(), folders.getString("object_name")));
+                    children.add(child);
+                }
+            } else if (parent.getType().equals("document")) {
+                IDfCollection documents = repositoryCon.query("select r_object_id, object_name from dm_document order by object_name");
+                while (documents.next()) {
+                    MyTreeItem child = new MyTreeItem(documents.getString("object_name"), "folder", new ImageView(
+                            new Image(getClass().getClassLoader().getResourceAsStream("document_16.png"))), String.format("%s/%s", parent.getPath(), documents.getString("object_name")));
+                    children.add(child);
+                }
+            }
+        } catch (DfException e) {
+            log.finest(e.getMessage());
+        }
+
+        return children;
     }
 }
--- src/main/java/nl/bos/controllers/RepositoryBrowser.java
+++ src/main/java/nl/bos/controllers/RepositoryBrowser.java
@@ -1,34 +1,29 @@
 package nl.bos.controllers;
 
-import com.documentum.fc.client.IDfCollection;
-import com.documentum.fc.common.DfException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import lombok.extern.java.Log;
 import nl.bos.MyTreeItem;
 import nl.bos.Repository;
 
-import java.io.File;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 @Log
-public class RepositoryBrowser implements Initializable, EventHandler<MouseEvent> {
+public class RepositoryBrowser implements Initializable {
     @FXML
-    private TreeView<File> treeview;
+    private TreeView<MyTreeItem> treeview;
     private Repository repositoryCon = Repository.getInstance();
-    private String currentPath = "";
 
     @FXML
     private void handleExit(ActionEvent actionEvent) {
@@ -40,102 +35,13 @@ public class RepositoryBrowser implements Initializable, EventHandler<MouseEvent
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         log.info(String.valueOf(location));
-        Repository repositoryCon = Repository.getInstance();
-
-        MyTreeItem rootItem = new MyTreeItem(repositoryCon.getRepositoryName(), "repository", new ImageView(new Image(getClass().getClassLoader().getResourceAsStream("repository_16.png"))));
+        MyTreeItem rootItem = new MyTreeItem(repositoryCon.getRepositoryName(), "repository", new ImageView(new Image(getClass().getClassLoader().getResourceAsStream("repository_16.png"))), String.format("/%s", repositoryCon.getRepositoryName()));
         rootItem.setExpanded(true);
-
-        try {
-            IDfCollection cabinets = repositoryCon.query("select r_object_id, object_name, is_private from dm_cabinet order by object_name");
-            while (cabinets.next()) {
-                MyTreeItem item = new MyTreeItem(cabinets.getString("object_name"), "cabinet", new ImageView(
-                        new Image(getClass().getClassLoader().getResourceAsStream("cabinet_16.png"))));
-                rootItem.getChildren().add(item);
-            }
-        } catch (DfException e) {
-            log.finest(e.getMessage());
-        }
-
-        treeview.setRoot(buildFileSystemBrowser2());
-        treeview.setOnMouseClicked(this);
-//        treeview.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-//            log.info(newValue.getValue());
-//            updateFolders(newValue);
-//        });
-//
-//        rootItem.expandedProperty().addListener((observable, oldValue, newValue) -> {
-//            log.info(String.valueOf(newValue));
-//            BooleanProperty bb = (BooleanProperty) observable;
-//            log.info(String.valueOf(bb.getBean()));
-//            MyTreeItem item = (MyTreeItem) bb.getBean();
-//            log.info(String.valueOf(item.getValue()));
-//        });
+        treeview.setRoot(buildFileSystemBrowser(rootItem));
     }
 
-    private void updateFolders(MyTreeItem parentItem) {
-        ObservableList<TreeItem<String>> children = parentItem.getChildren();
-        try {
-            IDfCollection folders = repositoryCon.query(String.format("select r_object_id, object_name from dm_folder where folder('%s') order by object_name", currentPath));
-            while (folders.next()) {
-                MyTreeItem item = new MyTreeItem(folders.getString("object_name"), "folder", new ImageView(
-                        new Image(getClass().getClassLoader().getResourceAsStream("folder_16.png"))));
-                children.add(item);
-            }
-        } catch (DfException e) {
-            log.finest(e.getMessage());
-        }
-        parentItem.setExpanded(true);
-    }
-
-    @Override
-    public void handle(MouseEvent event) {
-        if (event.getClickCount() == 2) {
-            TreeItem<File> item = treeview.getSelectionModel().getSelectedItem();
-            log.info(String.format("Open folder: %s ", item.getValue()));
-//            currentPath = String.format("%s/%s", currentPath, item.getValue());
-//            if(!item.isExpanded())
-//                updateFolders(item);
-        }
-    }
-
-    private TreeItem<String> buildFileSystemBrowser() {
-        return createNode("EP", "repository", new ImageView(new Image(getClass().getClassLoader().getResourceAsStream("repository_16.png"))));
-    }
-
-    private MyTreeItem createNode(String name, String type, ImageView imageView) {
-        return new MyTreeItem(name, type, imageView) {
-            private boolean isFirstTimeChildren = true;
-
-            @Override
-            public ObservableList<TreeItem<String>> getChildren() {
-                if (isFirstTimeChildren) {
-                    isFirstTimeChildren = false;
-                    super.getChildren().setAll(buildChildren(this));
-                }
-                return super.getChildren();
-            }
-
-            private ObservableList<MyTreeItem> buildChildren(MyTreeItem treeItem) {
-                if (treeItem != null && treeItem.isDirectory()) {
-                    String[] folders = treeItem.getFolders();
-                    if (folders != null) {
-                        ObservableList<MyTreeItem> children = FXCollections.observableArrayList();
-
-                        for (String folder : folders) {
-                            children.add(createNode(folder, "folder", new ImageView(new Image(getClass().getClassLoader().getResourceAsStream("folder_16.png")))));
-                        }
-
-                        return children;
-                    }
-                }
-
-                return FXCollections.emptyObservableList();
-            }
-        };
-    }
-
-    private TreeItem<File> buildFileSystemBrowser2() {
-        return createNode(new File("/"));
+    private TreeItem<MyTreeItem> buildFileSystemBrowser(MyTreeItem treeItem) {
+        return createNode(treeItem);
     }
 
     // This method creates a TreeItem to represent the given File. It does this
@@ -143,8 +49,8 @@ public class RepositoryBrowser implements Initializable, EventHandler<MouseEvent
     // anonymously, but this could be better abstracted by creating a
     // 'FileTreeItem' subclass of TreeItem. However, this is left as an exercise
     // for the reader.
-    private TreeItem<File> createNode(final File f) {
-        return new TreeItem<File>(f) {
+    private TreeItem<MyTreeItem> createNode(final MyTreeItem f) {
+        return new TreeItem<MyTreeItem>(f) {
             // We cache whether the File is a leaf or not. A File is a leaf if
             // it is not a directory and does not have any files contained within
             // it. We cache this as isLeaf() is called often, and doing the
@@ -161,7 +67,7 @@ public class RepositoryBrowser implements Initializable, EventHandler<MouseEvent
             private boolean isFirstTimeLeaf = true;
 
             @Override
-            public ObservableList<TreeItem<File>> getChildren() {
+            public ObservableList<TreeItem<MyTreeItem>> getChildren() {
                 if (isFirstTimeChildren) {
                     isFirstTimeChildren = false;
 
@@ -176,28 +82,24 @@ public class RepositoryBrowser implements Initializable, EventHandler<MouseEvent
             public boolean isLeaf() {
                 if (isFirstTimeLeaf) {
                     isFirstTimeLeaf = false;
-                    File f = getValue();
-                    isLeaf = f.isFile();
+                    MyTreeItem treeItem = getValue();
+                    isLeaf = !treeItem.isDirectory();
                 }
-
                 return isLeaf;
             }
 
-            private ObservableList<TreeItem<File>> buildChildren(TreeItem<File> TreeItem) {
-                File f = TreeItem.getValue();
-                if (f != null && f.isDirectory()) {
-                    File[] files = f.listFiles();
-                    if (files != null) {
-                        ObservableList<TreeItem<File>> children = FXCollections.observableArrayList();
-
-                        for (File childFile : files) {
+            private ObservableList<TreeItem<MyTreeItem>> buildChildren(TreeItem<MyTreeItem> parent) {
+                MyTreeItem treeItem = parent.getValue();
+                if (treeItem != null && treeItem.isDirectory()) {
+                    List<MyTreeItem> treeItems = treeItem.listObjects(treeItem);
+                    if (treeItems != null) {
+                        ObservableList<TreeItem<MyTreeItem>> children = FXCollections.observableArrayList();
+                        for (MyTreeItem childFile : treeItems) {
                             children.add(createNode(childFile));
                         }
-
                         return children;
                     }
                 }
-
                 return FXCollections.emptyObservableList();
             }
         };
