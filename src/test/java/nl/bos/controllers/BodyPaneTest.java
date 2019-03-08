88d8030a36ce98d4077f770cd95e86db8e898be9
Antal Bos <HetBenkt@gmail.com>
2019-03-08 16:48:32 +0100

    Some more standard tests for the class

--- src/test/java/nl/bos/controllers/BodyPaneTest.java
+++ src/test/java/nl/bos/controllers/BodyPaneTest.java
@@ -1,19 +1,20 @@
 package nl.bos.controllers;
 
 import com.sun.javafx.application.PlatformImpl;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import org.json.JSONObject;
 import org.junit.*;
-import org.mockito.Mock;
 import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.powermock.reflect.Whitebox;
+
+import static org.powermock.api.mockito.PowerMockito.mock;
 
 public class BodyPaneTest {
 
     @Rule
     public PowerMockRule rule = new PowerMockRule();
-
-    @Mock
-    private ChoiceBox<Object> mockedCmbHistory;
 
     @BeforeClass
     public static void javaFXInit() {
@@ -30,6 +31,7 @@ public class BodyPaneTest {
     public void getCmbHistory() {
         //given
         final BodyPane bodyPane = new BodyPane();
+        ChoiceBox mockedCmbHistory = mock(ChoiceBox.class);
         Whitebox.setInternalState(bodyPane, "cmbHistory", mockedCmbHistory);
 
         //when
@@ -41,14 +43,44 @@ public class BodyPaneTest {
 
     @Test
     public void getTaStatement() {
+        //given
+        final BodyPane bodyPane = new BodyPane();
+        TextArea mockedTaStatement = mock(TextArea.class);
+        Whitebox.setInternalState(bodyPane, "taStatement", mockedTaStatement);
+
+        //when
+        TextArea expected = bodyPane.getTaStatement();
+
+        //then
+        Assert.assertEquals(expected, mockedTaStatement);
     }
 
     @Test
     public void getJsonObject() {
+        //given
+        final BodyPane bodyPane = new BodyPane();
+        JSONObject mockedJsonObject = mock(JSONObject.class);
+        Whitebox.setInternalState(bodyPane, "jsonObject", mockedJsonObject);
+
+        //when
+        JSONObject expected = bodyPane.getJsonObject();
+
+        //then
+        Assert.assertEquals(expected, mockedJsonObject);
     }
 
     @Test
     public void getFxmlLoader() {
+        //given
+        final BodyPane bodyPane = new BodyPane();
+        FXMLLoader mockedFxmlLoader = mock(FXMLLoader.class);
+        Whitebox.setInternalState(bodyPane, "fxmlLoader", mockedFxmlLoader);
+
+        //when
+        FXMLLoader expected = bodyPane.getFxmlLoader();
+
+        //then
+        Assert.assertEquals(expected, mockedFxmlLoader);
     }
 
     @Test
