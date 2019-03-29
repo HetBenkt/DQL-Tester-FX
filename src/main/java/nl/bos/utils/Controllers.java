1077c07d9c6c99efe360b082ab8ee13ce84105af
besselaarw <wma.vd.besselaar@live.nl>
2019-03-29 10:49:19 +0100

    Controllers register class was missed during yesterday's commit. This register provides a single point of access to FX controllers

---
+++ src/main/java/nl/bos/utils/Controllers.java
@@ -0,0 +1,19 @@
+package nl.bos.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class Controllers {
+    private final static Map<String, Object> registeredControllers = new HashMap<>();
+
+    private Controllers() {
+    }
+
+    public static Object get(String controllerName) {
+        return registeredControllers.get(controllerName);
+    }
+
+    public static void put(String controllerName, Object controller) {
+        registeredControllers.put(controllerName, controller);
+    }
+}
