f50c372d321a3474f772414672ffb73f2f79c894
Antal Bos <HetBenkt@gmail.com>
2019-04-10 09:46:38 +0200

    1 last sonar lint message :)

--- src/main/java/nl/bos/utils/Controllers.java
+++ src/main/java/nl/bos/utils/Controllers.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public final class Controllers {
-    private final static Map<String, Object> registeredControllers = new HashMap<>();
+    private static final Map<String, Object> registeredControllers = new HashMap<>();
 
     private Controllers() {
     }
