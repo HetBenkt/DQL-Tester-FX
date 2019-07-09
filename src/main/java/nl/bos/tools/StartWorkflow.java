9038f5dd4569431e4199ac1f98957771ce4cd0c0
Antal Bos <HetBenkt@gmail.com>
2019-07-09 14:42:07 +0200

    Starting flow with single/multiple packages

--- src/main/java/nl/bos/tools/StartWorkflow.java
+++ src/main/java/nl/bos/tools/StartWorkflow.java
@@ -2,40 +2,31 @@ package nl.bos.tools;
 
 import com.documentum.com.DfClientX;
 import com.documentum.com.IDfClientX;
-import com.documentum.fc.client.*;
-import com.documentum.fc.common.*;
+import com.documentum.fc.client.IDfClient;
+import com.documentum.fc.client.IDfSession;
+import com.documentum.fc.client.IDfSessionManager;
+import com.documentum.fc.common.DfException;
+import com.documentum.fc.common.IDfId;
+import com.documentum.fc.common.IDfLoginInfo;
 
-import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class StartWorkflow {
-    private static final Logger LOGGER = Logger.getLogger(StartWorkflow.class.getName());
+abstract class StartWorkflow {
+    static final Logger LOGGER = Logger.getLogger(StartWorkflow.class.getName());
+    IDfSession session = null;
 
-    private StartWorkflow(String repository, String username, String password) {
+    StartWorkflow(String repository, String username, String password) {
         IDfSessionManager sessionManager = null;
-        IDfSession session = null;
 
         try {
             sessionManager = initSessionManager(repository, username, password);
             session = sessionManager.getSession(repository);
 
             LOGGER.info("Session created, now building the workflow");
-            IDfWorkflowBuilder wfBldrObj = session.newWorkflowBuilder(new DfId("4b12d591800015e0")); //dm_process.r_object_id
-            wfBldrObj.initWorkflow();
-            IDfId workflowId = wfBldrObj.runWorkflow();
 
-            IDfList objList = new DfList();
-            IDfSysObject sysObject = (IDfSysObject) session.newObject("dm_sysobject");
-            sysObject.setObjectName(String.format("test_%s", String.valueOf(new Date().getTime())));
-            sysObject.setSubject("process_test");
-            sysObject.save();
-            //select r_object_id, object_name, subject from dm_sysobject where subject = 'process_test'
-            //delete dm_sysobject objects where subject = 'process_test'
+            IDfId workflowId = startWorkflow();
 
-            objList.append(sysObject.getObjectId());
-            wfBldrObj.addPackage("Auto-Activity-1", "Input:0", "Package0",
-                    "dm_sysobject", null, false, objList);
             String message = String.format("Workflow is running with id: %s", workflowId.getId());
             LOGGER.info(message);
         } catch (DfException dfe) {
@@ -46,11 +37,9 @@ public class StartWorkflow {
         }
     }
 
-    public static void main(String[] args) {
-        new StartWorkflow(args[0], args[1], args[2]);
-    }
+    protected abstract IDfId startWorkflow() throws DfException;
 
-    private IDfSessionManager initSessionManager(String repository, String username, String password) throws DfException {
+    IDfSessionManager initSessionManager(String repository, String username, String password) throws DfException {
         IDfClientX clientx = new DfClientX();
         IDfClient client = clientx.getLocalClient();
 
@@ -65,3 +54,4 @@ public class StartWorkflow {
         return sessionManager;
     }
 }
+
